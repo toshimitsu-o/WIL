33ef891bcbc2bbe0df5d2697bf0f1b8aa92f8079
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-10-04 19:16:49 +1000

    add migration to create projects table

---
+++ laravel/database/migrations/2023_10_04_090725_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->text('name');
+            $table->text('description');
+            $table->integer('capacity');
+            $table->text('email');
+            $table->text('offer_year');
+            $table->text('offer_trimester');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
