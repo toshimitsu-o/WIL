2ed74f98af95b49631e65e06618ba084cb664cc6
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-10-04 20:35:10 +1000

    add class application

---
+++ laravel/app/Models/Application.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'project_id',
+        'justification',
+    ];
+
+    /**
+     * Get user assosiate with the application
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get project assosiate with the application
+     */
+    public function project() {
+        return $this->belongsTo(Project::class);
+    }
+}
